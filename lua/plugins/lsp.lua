14ad46fc6ed03d653b225de15e6e1a4a1c140f24
Themba Masina <themba@genba.co.za>
2023-08-22 08:35:08 +0200

    Add some plugins

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,31 @@
+return {
+  "VonHeikemen/lsp-zero.nvim",
+  dependencies = {
+    -- LSP Support
+    { "neovim/nvim-lspconfig" }, -- Required
+    { "williamboman/mason.nvim" }, -- Optional
+    { "williamboman/mason-lspconfig.nvim" }, -- Optional
+
+    -- Autocompletion
+    { "hrsh7th/nvim-cmp" }, -- Required
+    { "hrsh7th/cmp-nvim-lsp" }, -- Required
+    { "hrsh7th/cmp-buffer" }, -- Optional
+    { "hrsh7th/cmp-path" }, -- Optional
+    { "saadparwaiz1/cmp_luasnip" }, -- Optional
+    { "hrsh7th/cmp-nvim-lua" }, -- Optional
+
+    -- Snippets
+    { "L3MON4D3/LuaSnip" }, -- Required
+    { "rafamadriz/friendly-snippets" }, -- Optional
+  },
+  opts = function(_, opts)
+    local lsp = require("lsp-zero").preset({})
+
+    lsp.on_attach(function(client, bufnr)
+      -- see :help lsp-zero-keybindings
+      -- to learn the available actions
+      lsp.default_keymaps({ buffer = bufnr })
+    end)
+    lsp.setup()
+  end,
+}
