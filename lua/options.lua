ef0949a95a74385d6b2b6061889ac2e3e719670f
Themba Masina <themba@genba.co.za>
2024-02-23 15:33:09 +0200

    add comment plugin

--- lua/options.lua
+++ lua/options.lua
@@ -74,4 +74,4 @@ vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
 vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
 
 vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
-
+vim.keymap.set('n', '<leader>xx', vim.diagnostic.open_float)
