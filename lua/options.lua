f44ffbd4169f4f4e91eea00804a89773cc8c193c
Themba Masina <themba@genba.co.za>
2024-02-29 09:11:08 +0200

    add terminal toggler plugin

--- lua/options.lua
+++ lua/options.lua
@@ -56,6 +56,14 @@ keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) 
 keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
 keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
 
+keymap.set('n', '<leader>br', "<cmd>:e!<CR>", { desc = 'Reload Buffer' })
+
+-- vim.keymap.set('n', '<leader>c', ':bprev<CR>:bdelete #<CR>')
+keymap.set('n', '<leader>bd', ':bprev<CR>:bdelete #<CR>', { desc = 'Close Current Buffer' })
+keymap.set('n', '<leader>bP', "<cmd>:%bd<CR>", { desc = 'Close All Buffers' })
+keymap.set('n', '<leader>bC', "<cmd>%bd|e#|bd#<CR>", { desc = 'Close All Buffers But This' })
+vim.keymap.set('n', "<S-q>", '<cmd>:q<CR>', { desc = "Close Without Saving" })
+
 keymap.set("n", "<leader>t", "<cmd>sp<CR> <cmd>term<CR> <cmd>resize 20N<CR> i", { desc = "Open terminal" }) -- close current split window
 
 keymap.set("n", "<leader>p", '"_dp', { desc = "paste over without copying" })
@@ -72,6 +80,14 @@ vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
 vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
 vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
 vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+keymap.set('n', '<S-h>', ':bprev<CR>', { desc = 'Goto Previous Buffer', silent = true })
+keymap.set('n', '<S-l>', ':bnext<CR>', { desc = 'Goto Next Buffer', silent = true })
 
 vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
 vim.keymap.set('n', '<leader>xx', vim.diagnostic.open_float)
+
+-- move lines with alt + j/k
+vim.keymap.set('n', "<A-j>", ":m .+1<CR>==", { silent = true })
+vim.keymap.set('n', "<A-k>", ":m .-2<CR>==", { silent = true })
+
+vim.cmd [[autocmd BufWritePre * lua vim.lsp.buf.format()]]
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -15,6 +15,7 @@ return {
         },
       })
       local builtin = require("telescope.builtin")
+      vim.keymap.set('n', '<leader>fr', builtin.find_files, {desc = "Recent files" })
       vim.keymap.set('n', '<leader>ff', builtin.find_files, {desc = "Find files" })
       vim.keymap.set('n', '<leader><leader>', builtin.git_files, {desc = "Find git files" })
       vim.keymap.set('n', '<leader>fg', builtin.live_grep, {desc = "Live grep in files" })
